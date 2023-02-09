bd7a2044555721b7bddcc71092b292a8f3d51912
Juan David Hernández <juandhdez94@gmail.com>
2023-02-08 23:05:02 -0500

    Created extension to format with multiple formatters in VS Code

---
+++ src/formatter.ts
@@ -0,0 +1,117 @@
+import {
+    commands,
+    ExtensionContext,
+    workspace,
+    window as vsWindow,
+    OutputChannel,
+    TextDocument,
+    languages,
+    Range,
+    WorkspaceConfiguration,
+    ConfigurationTarget
+} from "vscode";
+import supportedLanguages from "./supported-languages";
+
+export default class Formatter {
+    private readonly FORMAT_DOCUMENT_ACTION = 'editor.action.formatDocument';
+    private readonly FORMAT_SELECTION_ACTION = 'editor.action.formatSelection';
+    private readonly OUTPUT_CHANNEL_NAME = 'Multi Formatter';
+
+    private formatAction: string;
+    private formatters: string[];
+    private logger: OutputChannel;
+
+    private defaultFormatter: string | undefined;
+    private config: WorkspaceConfiguration = {} as WorkspaceConfiguration;
+
+
+    constructor() {
+        this.logger = vsWindow.createOutputChannel(this.OUTPUT_CHANNEL_NAME);
+        
+        this.formatAction = this.FORMAT_DOCUMENT_ACTION;
+        const extensionConfig = workspace.getConfiguration('multiFormatter');
+        this.formatters = extensionConfig.get<string[]>('formatterList', []);
+    }
+
+    init(context: ExtensionContext) {
+        context.subscriptions.push(
+            commands.registerCommand('multiFormatter.formatSelection', this.formatSelection.bind(this)),
+            commands.registerCommand('multiFormatter.formatDocument', this.formatDocument.bind(this)),
+        );
+
+        this.logger.appendLine(`Registering formatter for supported languages`);
+        languages.registerDocumentRangeFormattingEditProvider(supportedLanguages, {
+            provideDocumentRangeFormattingEdits: this.selectFormattingAction.bind(this),
+        });
+    }
+
+    selectFormattingAction(document: TextDocument, range: Range) {
+        const firstLine = document.lineAt(0);
+        const lastLine = document.lineAt(document.lineCount - 1);
+        const fullRange = new Range(firstLine.range.start, lastLine.range.end);
+
+        if (range.isEmpty || range.isEqual(fullRange)) {
+            this.formatDocument();
+        } else {
+            this.formatSelection();
+        }
+
+        return [];
+    }
+
+    async formatSelection() {
+        this.formatAction = this.FORMAT_SELECTION_ACTION;
+        await this.format()
+    }
+
+    async formatDocument() {
+        this.formatAction = this.FORMAT_DOCUMENT_ACTION;
+        await this.format();
+    }
+
+    private async format() {
+        this.getFormattersForCurrentDocument();
+        await this.runFormatters();
+    }
+
+    getFormattersForCurrentDocument() {
+        const document = vsWindow.activeTextEditor?.document;
+        if (!document) {
+            this.logger.appendLine(`There is no document to get the language from`);
+            throw new Error('There is no document to get the language from');
+        }
+
+        // Some formatters don't work if the document is not directly active, so we active it here
+        vsWindow.showTextDocument(document);
+
+        this.config = workspace.getConfiguration('editor', document);
+        if (!this.config) {
+            this.logger.appendLine(`There is no config we can update`);
+            throw new Error('There is no config we can update');
+        }
+
+        this.defaultFormatter = this.config.get<string>('defaultFormatter',);
+
+        const extensionConfig = workspace.getConfiguration('multiFormatter', document);
+        this.formatters = extensionConfig.get<string[]>('formatterList', []);
+        if (this.formatters.length === 0
+            && this.defaultFormatter
+            && this.defaultFormatter !== 'Jota0222.multi-formatter'
+        ) {
+            this.logger.appendLine(`Added the default formatter ${this.defaultFormatter} to the list`);
+            this.formatters.push(this.defaultFormatter);
+        }
+    }
+
+    async runFormatters() {
+        for (const formatter of this.formatters) {
+            this.logger.appendLine(`Executing ${this.formatAction} with ${formatter}`);
+
+            await this.config.update('defaultFormatter', formatter, ConfigurationTarget.Workspace, true);
+            await commands.executeCommand(this.formatAction);
+        }
+
+        // Return back to the original configuration
+        await this.config.update('defaultFormatter', this.defaultFormatter, ConfigurationTarget.Workspace, true);
+    }
+};
---
+++ src/supported-languages.ts
@@ -0,0 +1,12 @@
+export default [
+    'php',
+    'javascript',
+    'typescript',
+    'html',
+    'python',
+    'vue',
+    'css',
+    'scss',
+    'jsx',
+    'javascriptreact'
+]
