d7eb91aa0530f5e5dad2710b77d68e9acd784dca
Juan David Hernández <juandhdez94@gmail.com>
2023-08-28 18:51:13 -0500

    Added validation before to save after format. As some users don't use format on save, we cannot save each time

--- src/formatter.ts
+++ src/formatter.ts
@@ -109,7 +109,10 @@ export default class Formatter {
             await commands.executeCommand(this.formatAction);
         }
 
-        await commands.executeCommand('workbench.action.files.save');
+        if (this.config.get<boolean>('formatOnSave')) {
+            await commands.executeCommand('workbench.action.files.save');
+        }
+
         // Return back to the original configuration
         await this.config.update('defaultFormatter', this.defaultFormatter, ConfigurationTarget.Workspace, true);
     }
